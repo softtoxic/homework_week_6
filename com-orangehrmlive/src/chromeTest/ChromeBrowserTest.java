169f806b78fd61a14d5d5d8531b6596306b31be8
softtoxic <lakhanijaydip09@gmail.com>
2023-06-20 23:09:25 +0100

    Homework done

---
+++ com-orangehrmlive/src/chromeTest/ChromeBrowserTest.java
@@ -0,0 +1,39 @@
+package chromeTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeBrowserTest {
+    public static void main(String[] args) {
+
+        String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+        // Launch the browser
+        WebDriver driver = new ChromeDriver();
+        // Open URL into browser
+        driver.get(baseUrl);
+        // Maximise the windows
+        driver.manage().window().maximize();
+        // Give to implicit wait to drive
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        // Get page source
+        System.out.println("Page source is : " + driver.getPageSource());
+        // Print title of the page
+        String title = driver.getTitle();
+        System.out.println(" Page title is : " +title);
+        // Get current URL of the page
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL of the page is : " + currentUrl);
+
+        // Get username element and send the keys
+        driver.findElement(By.name("username")).sendKeys("abcd1234");
+        // Get and send keys to password
+        WebElement password1 = driver.findElement(By.name("password"));
+        password1.sendKeys("xyz1234");
+        driver.close();
+
+    }
+}
